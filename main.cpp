7654d0cb5681a902c5f23bb307a2d444135e42c9
xaLi112 <74304902+xaLi112@users.noreply.github.com>
2021-05-26 19:46:16 +0200

    Edited Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int num, sticks, SecondPlayer;
+
+    bool winner = false;
+
+    cout << "This is a Game of Nim! Please enter a number between 1 to 3\n" << endl;
+    cin >> sticks;
+
+    while(sticks < 1 || sticks > 3 || sticks > num);
+    cout << "You have entered a wrong number\n"
+    << "Enter a number between 1 to 3 please!" << endl;
+    cin >> sticks;
+
 }
